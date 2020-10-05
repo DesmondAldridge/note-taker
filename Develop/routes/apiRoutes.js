fcbb5c1ab893082000995bb8d50072a9110f0a40
DesmondAldridge <67713101+DesmondAldridge@users.noreply.github.com>
2020-10-04 21:44:00 -0700

    Update apiRoutes.js
    Meh

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,8 +1,4 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
+
 
 const fs = require("fs");
 // var idMaker = idStamper();
@@ -34,13 +30,7 @@ module.exports = function(app) {
   });
 
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
+
 
   app.post("/api/notes", function(req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
@@ -56,9 +46,7 @@ module.exports = function(app) {
     }
   });
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
+
 
   app.post("/api/clear", function(req, res) {
     // Empty out the arrays of data
